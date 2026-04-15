e6ce44be5a19a867ea04c670ea869c6caeda57f4
Dtec-home <daudilinux@gmail.com>
2026-04-16 00:37:52 +0300

    feat: refactor Input component to enhance className handling and improve code readability

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -10,3 +10,11 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       className={cn(
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border border-2 border-gray-200/50 bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-emerald-500 focus-visible:ring-4 focus-visible:ring-emerald-500/30 focus-visible:ring-offset-0",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Input }
